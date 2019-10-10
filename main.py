034099ee0e64a7e1ab3134b38070423909b5a9d0
Elvira Pedregal Rodríguez <elvirapedregal@gmail.com>
2019-10-10 22:43:55 +0200

    first phyton

---
+++ main.py
@@ -0,0 +1,36 @@
+x=5
+y=2
+
+print (x+y)
+
+print (x+y+x)
+
+some_string = "hola mundo"
+
+print(some_string)
+
+string_a = "hola don pepito"
+string_b = " hola don josé"
+string_c = " pasó usted ya por casa"
+string_d = " por su casa yo pasé"
+
+print (string_a+string_b+string_c+string_d)
+
+#this is a comment
+#FUNCION INPUT
+
+user_name = input("Please enter your name")
+print ("hello" + user_name + "!")
+
+first_num= input("enter the first number:")
+second_num= input("enter the second number: ")
+
+print (int(first_num) + int(second_num))
+print (float(first_num) + float(second_num))
+
+mood = "nervous"
+
+if mood  == "happy":
+    print ("it is great to see you happy")
+else:
+    print("cheer up, mate!")

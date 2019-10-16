bb8098c420067fbf24ecb9fd4b34f8a2b5c38c51
Elvira Pedregal Rodríguez <elvirapedregal@gmail.com>
2019-10-16 17:38:16 +0200

    cuenta km

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-secret = 22
-guess = None
+#secret = 22
+#guess = None
 
-while guess!=secret:
-    guess = int(input("Guess the secret number : "))
+#while guess!=secret:
+    #guess = int(input("Guess the secret number : "))
 
-    if guess == secret:
-        print ("You guessed it - congratulations! It's number 22.")
-        break
-    elif guess> secret:
-        print ("try something smaller")
-    elif guess< secret:
-        print ("try something bigger")
+    #if guess == secret:
+        #print ("You guessed it - congratulations! It's number 22.")
+        #break
+    #elif guess> secret:
+        #print ("try something smaller")
+    #elif guess< secret:
+        #print ("try something bigger")
+
+
+millas = 0.62
+
+
+while True:
+
+    km = int(input("put your km"))
+    print = float(km * millas)
+
+
+
+
+
 
 
 

48297c35b71186a304f4497d68863000457d1eab
Elvira Pedregal Rodríguez <elvirapedregal@gmail.com>
2019-10-17 16:57:45 +0200

    pruebas y ejercicios

---
+++ probando.py
@@ -0,0 +1,30 @@
+#x= 2
+#y= 7
+
+#print (2+7)
+
+
+
+user_name=input("put your name please: ")
+print("hello"+ " " + user_name + "!")
+
+#first_num=int(input("enter your 1er number:"))
+#second_num=int(input("enter your 2º number:"))
+
+#print(first_num + second_num)
+
+#mood = input("how are you?:")
+
+#if mood == "happy":
+    print("It is great to see you happy!")
+#elif mood == "nervous":
+    print("Dont worry be happy,"+" "+ user_name)
+#elif mood == "sad":
+    print("Dont cry baby")
+#elif mood == "excited":
+    print("Tell me!!!")
+#elif mood == "relaxed":
+    print("Stay calm :)")
+#else:
+    print("I don't recognize this mood.")
+

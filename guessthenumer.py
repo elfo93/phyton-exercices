92898a79c6d0f67b2591374702295844e1724c18
Elvira Pedregal Rodríguez <elvirapedregal@gmail.com>
2019-10-17 20:09:39 +0200

    guess the number with list

--- guessthenumer.py
+++ guessthenumer.py
@@ -1,9 +1,14 @@
+import json
 import random
 
 secret = random.randint(1, 30)
-attempts = 0
+    attempts = 0
 
-with open("score_txt","r") as score.file:
+with open("score_list.txt","r") as score_file:
+    a = score_file.read() #siempre es string cuando se lee a menos que se le pase una funcion
+    score_list = json.loads(a) #transformamos en lista
+    print("top scores:" + str(score_list))
+
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1

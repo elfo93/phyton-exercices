2454b0bbe6d84f046d0b126078ad3a0194af5904
Elvira Pedregal Rodríguez <elvirapedregal@gmail.com>
2019-10-17 21:07:18 +0200

    score list

--- guessthenumer.py
+++ guessthenumer.py
@@ -2,11 +2,12 @@ import json
 import random
 
 secret = random.randint(1, 30)
-    attempts = 0
+attempts = 0
 
 with open("score_list.txt","r") as score_file:
     a = score_file.read() #siempre es string cuando se lee a menos que se le pase una funcion
     score_list = json.loads(a) #transformamos en lista
+    score_list.sort()
     print("top scores:" + str(score_list))
 
 while True:
@@ -16,9 +17,12 @@ while True:
     if guess == secret:
         print("You've guessed it - congratulations! It's number " + str(secret))
         print("Attempts needed: " + str(attempts))
+        score_list.append(attempts) #append es para añadir un elemento mas a la lista (el nº de intentos)
+        with open("score_list.txt","w") as score_file: #abrimos el archivo en modo escritura
+            jb= json.dumps(score_list) #pasamos la lista a string
+            score_file.write(jb)
         break
     elif guess > secret:
         print("Your guess is not correct... try something smaller")
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
-

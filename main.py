f4d36d1e44155d1c935256e10bf9036924d076be
elvira pedregal <55545344+elfo93@users.noreply.github.com>
2019-10-18 17:31:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+#secret = 22
+#guess = None
+
+#while guess!=secret:
+    #guess = int(input("Guess the secret number : "))
+
+    #if guess == secret:
+        #print ("You guessed it - congratulations! It's number 22.")
+        #break
+    #elif guess> secret:
+        #print ("try something smaller")
+    #elif guess< secret:
+        #print ("try something bigger")
+
+
+
+
+
+print ("heLLo, here you can convert km into miles,")
+
+while True:
+
+    km = input("please type your km: ")
+    km = float
+    miles= (0.62*km)
+    print(str(miles))
+
+
+
+proof = input("hello, put your name in capital letter:")
+print ("magicaly its will be turned in lower :)")
+print (str.lower (proof))
+
+
+
+
+
+
+
+
+
+
+
+
